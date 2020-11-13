0a6949f04d6bb584d63aa344648749aa15a220f2
derricklai33 <derricklai33@hotmail.com>
2020-11-13 21:34:32 +1100

    Updated routes to remove unnesscary routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root "welcome#index"
   get "/account", to: 'account#index', as: 'account' 
-  resources :sneakers
-  resources :apparels
+  resources :sneakers, only: [:index]
+  resources :apparels, only: [:index]
   resources :listings
+  get '/success/:id', to: 'listings#bought', as: 'bought'
 end
