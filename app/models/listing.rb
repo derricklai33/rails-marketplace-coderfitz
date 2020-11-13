efc5256c126ca92d3be85258d075d89df1d8e91c
derricklai33 <derricklai33@hotmail.com>
2020-11-13 21:00:13 +1100

    Added self function to set listing to inactive when a purchase is made

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -5,4 +5,8 @@ class Listing < ApplicationRecord
   accepts_nested_attributes_for :location
   enum category: {apparel: 0, sneaker: 1}
   enum size: {US5: 0, US6: 1, US7: 2, US8: 3, US9: 4, US10:5, US11: 6, US12: 7, US13: 8, XXS: 9, XS: 10, S: 11, M: 12, L: 13, XL:14, XXL: 15 }
+
+  def product_sold
+    self.sold = true
+  end
 end
