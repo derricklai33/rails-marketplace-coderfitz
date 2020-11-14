300af3d1665673191c623451be1d6e17f61725aa
derricklai33 <derricklai33@hotmail.com>
2020-11-14 14:14:49 +1100

    COnfigured controller to only show listings that are active in accounts page under active listing section

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -3,6 +3,6 @@ class AccountController < ApplicationController
   before_action :index
 
   def index
-    @listings = Listing.where(user_id: current_user.id)
+    @listings = Listing.where(user_id: current_user.id, sold: 0)
   end
 end
