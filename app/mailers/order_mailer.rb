af865bb442be54507e4fab01783ddcc60982c1d1
derricklai33 <derricklai33@hotmail.com>
2020-11-17 12:00:18 +1100

    Fixed up typo on action mailer and moved view files into correct folder

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -2,6 +2,6 @@ class OrderMailer < ApplicationMailer
   def send_order_mail(user, order)
     @user = user
     @order = order
-    mail( :to => @user.email, :subject => "Thanks for purchasing #{@order.listings.title}" )
+    mail( :to => @user.email, :subject => "Thanks for purchasing #{@order.listing.title}" )
   end
 end
