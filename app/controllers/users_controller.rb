c3089ada93f64dcb54e9f1863e8b44956dd918dc
derricklai33 <derricklai33@hotmail.com>
2020-11-17 21:46:24 +1100

    Added @sold_listings in user controller to show in views how many listings that are sold belongs to user. Styled show page with bootstrap

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @user_listings = @user.listings.where(sold: 0)
+    @sold_listings = @user.listings.where(sold: 1)
   end
 end
