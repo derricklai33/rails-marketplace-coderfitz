1786fafb3f5a81ae4bd36b05cd48a8399dc4d28b
derricklai33 <derricklai33@hotmail.com>
2020-11-18 13:50:46 +1100

    Minor edits on show page and fixed up eager loading on apparel and sneakers controller to optimise db query

--- app/controllers/apparels_controller.rb
+++ app/controllers/apparels_controller.rb
@@ -5,24 +5,24 @@ class ApparelsController < ApplicationController
   # Sorting by price and title
   def index
     if params[:order] == "Price: Low to High"
-      @apparel = Listing.order(price: :asc).where(category: 0, sold: 0).includes(:user, images_attachments: :blob)
+      @apparel = Listing.order(price: :asc).where(category: 0, sold: 0).includes(images_attachments: :blob)
       render :index
     elsif params[:order] == "Price: High to Low"
-      @apparel = Listing.order(price: :desc).where(category: 0, sold: 0).includes(:user, images_attachments: :blob)
+      @apparel = Listing.order(price: :desc).where(category: 0, sold: 0).includes(images_attachments: :blob)
       render :index
     elsif params[:order] == "Product: A to Z"
-      @apparel = Listing.order(title: :asc).where(category: 0, sold: 0).includes(:user, images_attachments: :blob)
+      @apparel = Listing.order(title: :asc).where(category: 0, sold: 0).includes(images_attachments: :blob)
       render :index
     elsif params[:order] == "Product: Z to A"
-      @apparel = Listing.order(title: :desc).where(category: 0, sold: 0).includes(:user, images_attachments: :blob)
+      @apparel = Listing.order(title: :desc).where(category: 0, sold: 0).includes(images_attachments: :blob)
       render :index
     end
 
     # Listing where only active and are apparels can be searched
     if params[:search].present?
-      @apparel = Listing.apparel_search_by(search_params).includes(:user, images_attachments: :blob)
+      @apparel = Listing.apparel_search_by(search_params).includes(images_attachments: :blob)
     else
-      @apparel = Listing.where(category: 0, sold: 0).includes(:user, images_attachments: :blob)
+      @apparel = Listing.where(category: 0, sold: 0).includes(images_attachments: :blob)
     end
   end
 
--- app/controllers/sneakers_controller.rb
+++ app/controllers/sneakers_controller.rb
@@ -6,25 +6,25 @@ class SneakersController < ApplicationController
   def index
     if params[:order].present?
       if params[:order] == "Price: Low to High"
-        @sneaker = Listing.order(price: :asc).where(category: 1, sold: 0).includes(:user, images_attachments: :blob)
+        @sneaker = Listing.order(price: :asc).where(category: 1, sold: 0).includes(images_attachments: :blob)
         render :index
       elsif params[:order] == "Price: High to Low"
-        @sneaker = Listing.order(price: :desc).where(category: 1, sold: 0).includes(:user, images_attachments: :blob)
+        @sneaker = Listing.order(price: :desc).where(category: 1, sold: 0).includes(images_attachments: :blob)
         render :index
       elsif params[:order] == "Product: A to Z"
-        @sneaker = Listing.order(title: :asc).where(category: 1, sold: 0).includes(:user, images_attachments: :blob)
+        @sneaker = Listing.order(title: :asc).where(category: 1, sold: 0).includes(images_attachments: :blob)
         render :index
       elsif params[:order] == "Product: Z to A"
-        @sneaker = Listing.order(title: :desc).where(category: 1, sold: 0).includes(:user, images_attachments: :blob)
+        @sneaker = Listing.order(title: :desc).where(category: 1, sold: 0).includes(images_attachments: :blob)
         render :index
       end
     end
     
     # Listing where only active and are sneakers can be searched
     if params[:search].present?
-      @sneaker = Listing.sneaker_search_by(search_params).includes(:user, images_attachments: :blob)
+      @sneaker = Listing.sneaker_search_by(search_params).includes(images_attachments: :blob)
     else
-      @sneaker = Listing.where(category: 1, sold: 0).includes(:user, images_attachments: :blob)
+      @sneaker = Listing.where(category: 1, sold: 0).includes(images_attachments: :blob)
     end
   end
 
