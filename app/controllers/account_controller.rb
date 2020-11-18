e828718bc0972add099a00995eee8708ca55d58f
derricklai33 <derricklai33@hotmail.com>
2020-11-18 17:31:44 +1100

    Configured appropriate flash messaged in controllers

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -3,7 +3,7 @@ class AccountController < ApplicationController
   before_action :index
 
   def index
-    @listings = Listing.where(user_id: current_user.id, sold: 0).includes(images_attachments: :blob)
-    @orders = Order.where(user_id: current_user.id).includes(:listing)
+    @listings = Listing.where(user_id: current_user.id, sold: 0)
+    @orders = Order.where(user: current_user).includes(:listing)
   end
 end
--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -6,13 +6,15 @@ class PaymentsController < ApplicationController
     @listing.product_sold
     @listing.save
     @order = Order.create(user_id: current_user.id, listing_id: @listing.id)
-    OrderMailer.send_order_mail(current_user, @order).deliver
+    OrderMailer.send_order_mail(current_user, @order).deliver 
+    redirect_to root_path, success: "Purchase completed! Check your email for your order summary!"
   end
 
-  def webhook
-    payment_id= params[:data][:object][:payment_intent]
-    payment = Stripe::PaymentIntent.retrieve(payment_id)
-    dish_id = payment.metadata.dish_id
-    user_id = payment.metadata.user_id
-  end
+  # Unable to get webhook to work
+  # def webhook
+  #   payment_id= params[:data][:object][:payment_intent]
+  #   payment = Stripe::PaymentIntent.retrieve(payment_id)
+  #   dish_id = payment.metadata.dish_id
+  #   user_id = payment.metadata.user_id
+  # end
 end
